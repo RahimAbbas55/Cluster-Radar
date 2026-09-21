b4b000e00e69d7edc00ac93e6394f26ac96a188a
RahimAbbas55 <irahimsindhu@gmail.com>
2026-09-22 02:16:35 +0500

    feat(gcp): point Helm chart at Artifact Registry images, disable deletion_protection for teardown

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -2,7 +2,8 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  # we manage node pools separately, so remove the default one
+  deletion_protection = false
+
   remove_default_node_pool = true
   initial_node_count       = 1
 
